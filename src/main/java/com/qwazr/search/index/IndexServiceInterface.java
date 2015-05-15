1e6282822dd1a2bf55e4a59f2f80e8ca58bdc9cc
Emmanuel Keller <ekeller@open-search-server.com>
2015-05-15 08:55:01 +0200

    Branding

--- src/main/java/com/qwazr/search/index/IndexServiceInterface.java
+++ src/main/java/com/qwazr/search/index/IndexServiceInterface.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 OpenSearchServer Inc.
+ * Copyright 2015 Emmanuel Keller / QWAZR
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
