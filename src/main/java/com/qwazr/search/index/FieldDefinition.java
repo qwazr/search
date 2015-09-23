ff546878a175867069a9f08b340539eb4b506ef7
Emmanuel Keller <ekeller@open-search-server.com>
2015-09-24 01:48:08 +0200

    Code format

--- src/main/java/com/qwazr/search/index/FieldDefinition.java
+++ src/main/java/com/qwazr/search/index/FieldDefinition.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Emmanuel Keller / QWAZR
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,176 +35,175 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class FieldDefinition {
 
-	public final String analyzer;
-	public final Boolean tokenized;
-	public final Boolean stored;
-	public final Boolean store_termvectors;
-	public final Boolean store_termvector_offsets;
-	public final Boolean store_termvector_positions;
-	public final Boolean store_termvector_payloads;
-	public final Boolean omit_norms;
-	public final FieldType.NumericType numeric_type;
-	public final IndexOptions index_options;
-	public final DocValuesType docvalues_type;
+    public final String analyzer;
+    public final Boolean tokenized;
+    public final Boolean stored;
+    public final Boolean store_termvectors;
+    public final Boolean store_termvector_offsets;
+    public final Boolean store_termvector_positions;
+    public final Boolean store_termvector_payloads;
+    public final Boolean omit_norms;
+    public final FieldType.NumericType numeric_type;
+    public final IndexOptions index_options;
+    public final DocValuesType docvalues_type;
 
-	public enum Template {
-		DoubleField, FloatField, IntField, LongField, NumericDocValuesField, FloatDocValuesField, DoubleDocValuesField,
-		SortedDocValuesField, SortedNumericDocValuesField, SortedDoubleDocValuesField, SortedFloatDocValuesField,
-		SortedSetDocValuesField, StoredField, StringField, TextField, FacetField, SortedSetDocValuesFacetField,
-		SortedSetMultiDocValuesFacetField;
+    public enum Template {
+	DoubleField, FloatField, IntField, LongField, NumericDocValuesField, FloatDocValuesField, DoubleDocValuesField,
+	SortedDocValuesField, SortedNumericDocValuesField, SortedDoubleDocValuesField, SortedFloatDocValuesField,
+	SortedSetDocValuesField, StoredField, StringField, TextField, FacetField, SortedSetDocValuesFacetField,
+	SortedSetMultiDocValuesFacetField;
+    }
+
+    public final Template template;
+
+    public FieldDefinition() {
+	analyzer = null;
+	tokenized = null;
+	stored = null;
+	store_termvectors = null;
+	store_termvector_offsets = null;
+	store_termvector_positions = null;
+	store_termvector_payloads = null;
+	omit_norms = null;
+	numeric_type = null;
+	index_options = null;
+	docvalues_type = null;
+	template = null;
+    }
+
+    private Number checkNumberType(String fieldName, Object value) {
+	if (!(value instanceof Number))
+	    throw new IllegalArgumentException("Wrong value type for the field: " + fieldName + " - " +
+			    value.getClass().getSimpleName());
+	return (Number) value;
+    }
+
+    private BytesRef checkStringBytesRef(Object value) {
+	return new BytesRef(value.toString());
+    }
+
+    void putNewField(String fieldName, Object value, Document doc) {
+	if (value == null)
+	    return;
+	Field field = null;
+	Field.Store store = (stored != null && stored) ? Field.Store.YES : Field.Store.NO;
+	if (template != null) {
+	    switch (template) {
+	    case DoubleField:
+		field = new DoubleField(fieldName, checkNumberType(fieldName, value).doubleValue(), store);
+		break;
+	    case FloatField:
+		field = new FloatField(fieldName, checkNumberType(fieldName, value).floatValue(), store);
+		break;
+	    case IntField:
+		field = new IntField(fieldName, checkNumberType(fieldName, value).intValue(), store);
+		break;
+	    case LongField:
+		field = new LongField(fieldName, checkNumberType(fieldName, value).longValue(), store);
+		break;
+	    case NumericDocValuesField:
+		field = new NumericDocValuesField(fieldName, checkNumberType(fieldName, value).longValue());
+		break;
+	    case FloatDocValuesField:
+		field = new FloatDocValuesField(fieldName, checkNumberType(fieldName, value).floatValue());
+		break;
+	    case DoubleDocValuesField:
+		field = new DoubleDocValuesField(fieldName, checkNumberType(fieldName, value).doubleValue());
+		break;
+	    case SortedDocValuesField:
+		field = new SortedDocValuesField(fieldName, checkStringBytesRef(value));
+		break;
+	    case SortedNumericDocValuesField:
+		field = new SortedNumericDocValuesField(fieldName, checkNumberType(fieldName, value).longValue());
+		break;
+	    case SortedDoubleDocValuesField:
+		field = new SortedNumericDocValuesField(fieldName,
+				NumericUtils.doubleToSortableLong(checkNumberType(fieldName, value).doubleValue()));
+		break;
+	    case SortedFloatDocValuesField:
+		field = new SortedNumericDocValuesField(fieldName,
+				NumericUtils.floatToSortableInt(checkNumberType(fieldName, value).floatValue()));
+		break;
+	    case SortedSetDocValuesField:
+		field = new SortedSetDocValuesField(fieldName, checkStringBytesRef(value));
+		break;
+	    case StoredField:
+		if (value instanceof String)
+		    field = new StoredField(fieldName, (String) value);
+		else if (value instanceof Integer)
+		    field = new StoredField(fieldName, (int) value);
+		else if (value instanceof Long)
+		    field = new StoredField(fieldName, (long) value);
+		else if (value instanceof Float)
+		    field = new StoredField(fieldName, (float) value);
+		else if (value instanceof Double)
+		    field = new StoredField(fieldName, (double) value);
+		else
+		    field = new StoredField(fieldName, value.toString());
+		break;
+	    case StringField:
+		field = new StringField(fieldName, value.toString(), store);
+		break;
+	    case TextField:
+		field = new TextField(fieldName, value.toString(), store);
+		break;
+	    case FacetField:
+		field = new FacetField(fieldName, value.toString());
+		break;
+	    case SortedSetDocValuesFacetField:
+	    case SortedSetMultiDocValuesFacetField:
+		field = new SortedSetDocValuesFacetField(fieldName, value.toString());
+		break;
+	    }
+	}
+	if (field == null) {
+	    FieldType type = new FieldType();
+	    if (stored != null)
+		type.setStored(stored);
+	    if (tokenized != null)
+		type.setTokenized(tokenized);
+	    if (store_termvectors != null)
+		type.setStoreTermVectors(store_termvectors);
+	    if (store_termvector_offsets != null)
+		type.setStoreTermVectorOffsets(store_termvector_offsets);
+	    if (store_termvector_positions != null)
+		type.setStoreTermVectorPositions(store_termvector_positions);
+	    if (store_termvector_payloads != null)
+		type.setStoreTermVectorPayloads(store_termvector_payloads);
+	    if (omit_norms != null)
+		type.setOmitNorms(omit_norms);
+	    if (numeric_type != null)
+		type.setNumericType(numeric_type);
+	    if (index_options != null)
+		type.setIndexOptions(index_options);
+	    if (docvalues_type != null)
+		type.setDocValuesType(docvalues_type);
+
+	    field = new Field(fieldName, value.toString(), type);
 	}
 
-	public final Template template;
+	doc.add(field);
 
-	public FieldDefinition() {
-		analyzer = null;
-		tokenized = null;
-		stored = null;
-		store_termvectors = null;
-		store_termvector_offsets = null;
-		store_termvector_positions = null;
-		store_termvector_payloads = null;
-		omit_norms = null;
-		numeric_type = null;
-		index_options = null;
-		docvalues_type = null;
-		template = null;
-	}
+    }
 
-	private Number checkNumberType(String fieldName, Object value) {
-		if (!(value instanceof Number))
-			throw new IllegalArgumentException("Wrong value type for the field: " + fieldName + " - " +
-					value.getClass().getSimpleName());
-		return (Number) value;
-	}
+    public final static Object getValue(IndexableField field) {
+	if (field == null)
+	    return null;
+	String s = field.stringValue();
+	if (s != null)
+	    return s;
+	Number n = field.numericValue();
+	if (n != null)
+	    return n;
+	return null;
+    }
 
-	private BytesRef checkStringBytesRef(Object value) {
-		return new BytesRef(value.toString());
-	}
+    public final static TypeReference<Map<String, FieldDefinition>> MapStringFieldTypeRef = new TypeReference<Map<String, FieldDefinition>>() {
+    };
 
-	void putNewField(String fieldName, Object value, Document doc) {
-		if (value == null)
-			return;
-		Field field = null;
-		Field.Store store = (stored != null && stored) ? Field.Store.YES : Field.Store.NO;
-		if (template != null) {
-			switch (template) {
-				case DoubleField:
-					field = new DoubleField(fieldName, checkNumberType(fieldName, value).doubleValue(), store);
-					break;
-				case FloatField:
-					field = new FloatField(fieldName, checkNumberType(fieldName, value).floatValue(), store);
-					break;
-				case IntField:
-					field = new IntField(fieldName, checkNumberType(fieldName, value).intValue(), store);
-					break;
-				case LongField:
-					field = new LongField(fieldName, checkNumberType(fieldName, value).longValue(), store);
-					break;
-				case NumericDocValuesField:
-					field = new NumericDocValuesField(fieldName, checkNumberType(fieldName, value).longValue());
-					break;
-				case FloatDocValuesField:
-					field = new FloatDocValuesField(fieldName, checkNumberType(fieldName, value).floatValue());
-					break;
-				case DoubleDocValuesField:
-					field = new DoubleDocValuesField(fieldName, checkNumberType(fieldName, value).doubleValue());
-					break;
-				case SortedDocValuesField:
-					field = new SortedDocValuesField(fieldName, checkStringBytesRef(value));
-					break;
-				case SortedNumericDocValuesField:
-					field = new SortedNumericDocValuesField(fieldName, checkNumberType(fieldName, value).longValue());
-					break;
-				case SortedDoubleDocValuesField:
-					field = new SortedNumericDocValuesField(fieldName,
-							NumericUtils.doubleToSortableLong(checkNumberType(fieldName, value).doubleValue()));
-					break;
-				case SortedFloatDocValuesField:
-					field = new SortedNumericDocValuesField(fieldName,
-							NumericUtils.floatToSortableInt(checkNumberType(fieldName, value).floatValue()));
-					break;
-				case SortedSetDocValuesField:
-					field = new SortedSetDocValuesField(fieldName, checkStringBytesRef(value));
-					break;
-				case StoredField:
-					if (value instanceof String)
-						field = new StoredField(fieldName, (String) value);
-					else if (value instanceof Integer)
-						field = new StoredField(fieldName, (int) value);
-					else if (value instanceof Long)
-						field = new StoredField(fieldName, (long) value);
-					else if (value instanceof Float)
-						field = new StoredField(fieldName, (float) value);
-					else if (value instanceof Double)
-						field = new StoredField(fieldName, (double) value);
-					break;
-				case StringField:
-					field = new StringField(fieldName, value.toString(), store);
-					break;
-				case TextField:
-					field = new TextField(fieldName, value.toString(), store);
-					break;
-				case FacetField:
-					field = new FacetField(fieldName, value.toString());
-					break;
-				case SortedSetDocValuesFacetField:
-				case SortedSetMultiDocValuesFacetField:
-					field = new SortedSetDocValuesFacetField(fieldName, value.toString());
-					break;
-			}
-		}
-		if (field == null) {
-			FieldType type = new FieldType();
-			if (stored != null)
-				type.setStored(stored);
-			if (tokenized != null)
-				type.setTokenized(tokenized);
-			if (store_termvectors != null)
-				type.setStoreTermVectors(store_termvectors);
-			if (store_termvector_offsets != null)
-				type.setStoreTermVectorOffsets(store_termvector_offsets);
-			if (store_termvector_positions != null)
-				type.setStoreTermVectorPositions(store_termvector_positions);
-			if (store_termvector_payloads != null)
-				type.setStoreTermVectorPayloads(store_termvector_payloads);
-			if (omit_norms != null)
-				type.setOmitNorms(omit_norms);
-			if (numeric_type != null)
-				type.setNumericType(numeric_type);
-			if (index_options != null)
-				type.setIndexOptions(index_options);
-			if (docvalues_type != null)
-				type.setDocValuesType(docvalues_type);
-
-			field = new Field(fieldName, value.toString(), type);
-		}
-
-		doc.add(field);
-
-		if (field.fieldType().docValuesType() != null && store == Field.Store.YES)
-			doc.add(new StoredField(fieldName, value.toString()));
-	}
-
-	public final static Object getValue(IndexableField field) {
-		if (field == null)
-			return null;
-		String s = field.stringValue();
-		if (s != null)
-			return s;
-		Number n = field.numericValue();
-		if (n != null)
-			return n;
-		return null;
-	}
-
-	public final static TypeReference<Map<String, FieldDefinition>> MapStringFieldTypeRef =
-			new TypeReference<Map<String, FieldDefinition>>() {
-			};
-
-	public static Map<String, FieldDefinition> newFieldMap(String jsonString) throws IOException {
-		if (StringUtils.isEmpty(jsonString))
-			return null;
-		return JsonMapper.MAPPER.readValue(jsonString, MapStringFieldTypeRef);
-	}
+    public static Map<String, FieldDefinition> newFieldMap(String jsonString) throws IOException {
+	if (StringUtils.isEmpty(jsonString))
+	    return null;
+	return JsonMapper.MAPPER.readValue(jsonString, MapStringFieldTypeRef);
+    }
 }
--- src/main/java/com/qwazr/search/index/ResultDefinition.java
+++ src/main/java/com/qwazr/search/index/ResultDefinition.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Emmanuel Keller / QWAZR
- * <p>
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,149 +37,147 @@ import java.util.Map;
 @JsonInclude(Include.NON_EMPTY)
 public class ResultDefinition {
 
-	final public Map<String, Long> timer;
-	final public long total_hits;
-	final public List<ResultDocument> documents;
-	final public Map<String, Map<String, Number>> facets;
+    final public Map<String, Long> timer;
+    final public long total_hits;
+    final public List<ResultDocument> documents;
+    final public Map<String, Map<String, Number>> facets;
 
-	public ResultDefinition() {
-		this.timer = null;
-		this.total_hits = 0;
-		this.documents = null;
-		this.facets = null;
+    public ResultDefinition() {
+	this.timer = null;
+	this.total_hits = 0;
+	this.documents = null;
+	this.facets = null;
+    }
+
+    public class ResultDocument {
+
+	final private ScoreDoc scoreDoc;
+	final public Map<String, Object> fields;
+	final public Map<String, String> postings_highlights;
+
+	public ResultDocument() {
+	    scoreDoc = null;
+	    fields = null;
+	    postings_highlights = null;
 	}
 
-	public class ResultDocument {
+	private ResultDocument(int pos, ScoreDoc[] scoreDocs, IndexSearcher searcher, QueryDefinition queryDef,
+			Map<String, String[]> postingsHighlightsMap) throws IOException {
 
-		final private ScoreDoc scoreDoc;
-		final public Map<String, Object> fields;
-		final public Map<String, String> postings_highlights;
+	    scoreDoc = scoreDocs[pos];
+	    Document document = searcher.doc(scoreDoc.doc, queryDef.returned_fields);
 
-		public ResultDocument() {
-			scoreDoc = null;
-			fields = null;
-			postings_highlights = null;
+	    // Build field map
+	    fields = new LinkedHashMap<String, Object>();
+	    for (IndexableField field : document) {
+		Object newValue = FieldDefinition.getValue(field);
+		if (newValue == null)
+		    continue;
+		Object oldValue = fields.get(field.name());
+		if (oldValue == null) {
+		    fields.put(field.name(), newValue);
+		    continue;
 		}
-
-		private ResultDocument(int pos, ScoreDoc[] scoreDocs, IndexSearcher searcher, QueryDefinition queryDef,
-							   Map<String, String[]> postingsHighlightsMap)
-				throws IOException {
-
-			scoreDoc = scoreDocs[pos];
-			Document document = searcher.doc(scoreDoc.doc, queryDef.returned_fields);
-
-			// Build field map
-			fields = new LinkedHashMap<String, Object>();
-			for (IndexableField field : document) {
-				Object newValue = FieldDefinition.getValue(field);
-				if (newValue == null)
-					continue;
-				Object oldValue = fields.get(field.name());
-				if (oldValue == null) {
-					fields.put(field.name(), newValue);
-					continue;
-				}
-				if (oldValue instanceof List<?>) {
-					((List<Object>) oldValue).add(newValue);
-					continue;
-				}
-				List<Object> list = new ArrayList<Object>(2);
-				list.add(oldValue);
-				list.add(newValue);
-				fields.put(field.name(), list);
-			}
-
-			// Build postings hightlights
-			if (queryDef.postings_highlighter != null && postingsHighlightsMap != null) {
-				postings_highlights = new LinkedHashMap<String, String>();
-				for (String field : queryDef.postings_highlighter.keySet()) {
-					String[] highlights = postingsHighlightsMap.get(field);
-					if (highlights == null)
-						continue;
-					String highlight = highlights[pos];
-					if (highlight == null)
-						continue;
-					postings_highlights.put(field, highlight);
-				}
-			} else
-				postings_highlights = null;
+		if (oldValue instanceof List<?>) {
+		    ((List<Object>) oldValue).add(newValue);
+		    continue;
 		}
+		List<Object> list = new ArrayList<Object>(2);
+		list.add(oldValue);
+		list.add(newValue);
+		fields.put(field.name(), list);
+	    }
 
-		public Float getScore() {
-			return scoreDoc != null ? scoreDoc.score : null;
+	    // Build postings hightlights
+	    if (queryDef.postings_highlighter != null && postingsHighlightsMap != null) {
+		postings_highlights = new LinkedHashMap<String, String>();
+		for (String field : queryDef.postings_highlighter.keySet()) {
+		    String[] highlights = postingsHighlightsMap.get(field);
+		    if (highlights == null)
+			continue;
+		    String highlight = highlights[pos];
+		    if (highlight == null)
+			continue;
+		    postings_highlights.put(field, highlight);
 		}
-
-		public Integer getDoc() {
-			return scoreDoc != null ? scoreDoc.doc : null;
-		}
-
-		public Integer getShard_index() {
-			return scoreDoc != null ? scoreDoc.shardIndex : null;
-		}
-
-		public Map<String, Object> getFields() {
-			return fields;
-		}
-
-		public Map<String, String> getPostings_highlights() {
-			return postings_highlights;
-		}
+	    } else
+		postings_highlights = null;
 	}
 
-	public ResultDefinition(TimeTracker timeTracker, IndexSearcher searcher, TopDocs topDocs, QueryDefinition queryDef,
-							Facets facets, Map<String, String[]> postingsHighlightsMap)
-			throws IOException {
-		total_hits = topDocs.totalHits;
-		int pos = queryDef.start == null ? 0 : queryDef.start;
-		int end = queryDef.getEnd();
-		documents = new ArrayList<ResultDocument>();
-		ScoreDoc[] docs = topDocs.scoreDocs;
-		IndexReader reader = searcher.getIndexReader();
-		while (pos < total_hits && pos < end)
-			documents.add(new ResultDocument(pos++, docs, searcher, queryDef, postingsHighlightsMap));
-		timeTracker.next("returned_fields");
-		this.facets = facets != null && queryDef != null ? buildFacets(queryDef.facets, facets) : null;
-		timeTracker.next("facet_fields");
-		this.timer = timeTracker == null ? null : timeTracker.getMap();
+	public Float getScore() {
+	    return scoreDoc != null ? scoreDoc.score : null;
 	}
 
-	private Map<String, Map<String, Number>> buildFacets(Map<String, QueryDefinition.Facet> facetsDef,
-														 Facets facets) throws IOException {
-		Map<String, Map<String, Number>> facetResults = new LinkedHashMap<String, Map<String, Number>>();
-		for (Map.Entry<String, QueryDefinition.Facet> entry : facetsDef.entrySet()) {
-			String dim = entry.getKey();
-			Map<String, Number> facetMap = buildFacet(dim, entry.getValue(), facets);
-			if (facetMap != null)
-				facetResults.put(dim, facetMap);
-		}
-		return facetResults;
+	public Integer getDoc() {
+	    return scoreDoc != null ? scoreDoc.doc : null;
 	}
 
-	private static Map<String, Number> buildFacet(String dim, QueryDefinition.Facet facet, Facets facets)
-			throws IOException {
-		int top = facet.top == null ? 10 : facet.top;
-		LinkedHashMap<String, Number> facetMap = new LinkedHashMap<String, Number>();
-		FacetResult facetResult = facets.getTopChildren(top, dim);
-		if (facetResult == null || facetResult.labelValues == null)
-			return null;
-		for (LabelAndValue lv : facetResult.labelValues)
-			facetMap.put(lv.label, lv.value);
-		return facetMap;
+	public Integer getShard_index() {
+	    return scoreDoc != null ? scoreDoc.shardIndex : null;
 	}
 
-	public long getTotal_hits() {
-		return total_hits;
+	public Map<String, Object> getFields() {
+	    return fields;
 	}
 
-	public List<ResultDocument> getDocuments() {
-		return documents;
+	public Map<String, String> getPostings_highlights() {
+	    return postings_highlights;
 	}
+    }
 
-	public Map<String, Map<String, Number>> getFacets() {
-		return facets;
-	}
+    public ResultDefinition(TimeTracker timeTracker, IndexSearcher searcher, TopDocs topDocs, QueryDefinition queryDef,
+		    Facets facets, Map<String, String[]> postingsHighlightsMap) throws IOException {
+	total_hits = topDocs.totalHits;
+	int pos = queryDef.start == null ? 0 : queryDef.start;
+	int end = queryDef.getEnd();
+	documents = new ArrayList<ResultDocument>();
+	ScoreDoc[] docs = topDocs.scoreDocs;
+	IndexReader reader = searcher.getIndexReader();
+	while (pos < total_hits && pos < end)
+	    documents.add(new ResultDocument(pos++, docs, searcher, queryDef, postingsHighlightsMap));
+	timeTracker.next("returned_fields");
+	this.facets = facets != null && queryDef != null ? buildFacets(queryDef.facets, facets) : null;
+	timeTracker.next("facet_fields");
+	this.timer = timeTracker == null ? null : timeTracker.getMap();
+    }
 
-	public Map<String, Long> getTimer() {
-		return timer;
+    private Map<String, Map<String, Number>> buildFacets(Map<String, QueryDefinition.Facet> facetsDef, Facets facets)
+		    throws IOException {
+	Map<String, Map<String, Number>> facetResults = new LinkedHashMap<String, Map<String, Number>>();
+	for (Map.Entry<String, QueryDefinition.Facet> entry : facetsDef.entrySet()) {
+	    String dim = entry.getKey();
+	    Map<String, Number> facetMap = buildFacet(dim, entry.getValue(), facets);
+	    if (facetMap != null)
+		facetResults.put(dim, facetMap);
 	}
+	return facetResults;
+    }
+
+    private static Map<String, Number> buildFacet(String dim, QueryDefinition.Facet facet, Facets facets)
+		    throws IOException {
+	int top = facet.top == null ? 10 : facet.top;
+	LinkedHashMap<String, Number> facetMap = new LinkedHashMap<String, Number>();
+	FacetResult facetResult = facets.getTopChildren(top, dim);
+	if (facetResult == null || facetResult.labelValues == null)
+	    return null;
+	for (LabelAndValue lv : facetResult.labelValues)
+	    facetMap.put(lv.label, lv.value);
+	return facetMap;
+    }
+
+    public long getTotal_hits() {
+	return total_hits;
+    }
+
+    public List<ResultDocument> getDocuments() {
+	return documents;
+    }
+
+    public Map<String, Map<String, Number>> getFacets() {
+	return facets;
+    }
+
+    public Map<String, Long> getTimer() {
+	return timer;
+    }
 }
