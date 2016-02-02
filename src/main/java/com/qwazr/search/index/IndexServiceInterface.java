bcebe52b0b4d1137f10c78184880b8339f520993
Emmanuel Keller <ekeller@open-search-server.com>
2016-02-02 23:36:30 +0100

    Add missing cient builder

--- src/main/java/com/qwazr/search/index/IndexServiceInterface.java
+++ src/main/java/com/qwazr/search/index/IndexServiceInterface.java
@@ -23,7 +23,6 @@ import com.qwazr.utils.server.ServiceName;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URISyntaxException;
 import java.util.LinkedHashMap;
@@ -46,7 +45,7 @@ public interface IndexServiceInterface extends ServiceInterface {
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	SchemaSettingsDefinition createUpdateSchema(@PathParam("schema_name") String schema_name,
-					SchemaSettingsDefinition settings);
+			SchemaSettingsDefinition settings);
 
 	@GET
 	@Path("/")
@@ -66,93 +65,93 @@ public interface IndexServiceInterface extends ServiceInterface {
 	@Path("/{schema_name}/{index_name}")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	IndexStatus createUpdateIndex(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name);
+			@PathParam("index_name") String index_name);
 
 	@POST
 	@Path("/{schema_name}/{index_name}")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	IndexStatus createUpdateIndex(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, IndexSettingsDefinition settings);
+			@PathParam("index_name") String index_name, IndexSettingsDefinition settings);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/fields")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	LinkedHashMap<String, FieldDefinition> getFields(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name);
+			@PathParam("index_name") String index_name);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/fields")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	LinkedHashMap<String, FieldDefinition> setFields(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, LinkedHashMap<String, FieldDefinition> fields);
+			@PathParam("index_name") String index_name, LinkedHashMap<String, FieldDefinition> fields);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/fields/{field_name}/analyzer/query")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	List<TermDefinition> doAnalyzeQuery(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, @PathParam("field_name") String field_name,
-					@QueryParam("text") String text);
+			@PathParam("index_name") String index_name, @PathParam("field_name") String field_name,
+			@QueryParam("text") String text);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/fields/{field_name}/analyzer/index")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	List<TermDefinition> doAnalyzeIndex(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, @PathParam("field_name") String field_name,
-					@QueryParam("text") String text);
+			@PathParam("index_name") String index_name, @PathParam("field_name") String field_name,
+			@QueryParam("text") String text);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/fields/{field_name}")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	FieldDefinition getField(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					@PathParam("field_name") String field_name);
+			@PathParam("field_name") String field_name);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/fields/{field_name}")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	FieldDefinition setField(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					@PathParam("field_name") String field_name, FieldDefinition fields);
+			@PathParam("field_name") String field_name, FieldDefinition fields);
 
 	@DELETE
 	@Path("/{schema_name}/{index_name}/fields/{field_name}")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response deleteField(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					@PathParam("field_name") String field_name);
+			@PathParam("field_name") String field_name);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/analyzers")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	LinkedHashMap<String, AnalyzerDefinition> getAnalyzers(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name);
+			@PathParam("index_name") String index_name);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/analyzers/{analyzer_name}")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	AnalyzerDefinition getAnalyzer(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, @PathParam("analyzer_name") String analyzer_name);
+			@PathParam("index_name") String index_name, @PathParam("analyzer_name") String analyzer_name);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/analyzers/{analyzer_name}")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	AnalyzerDefinition setAnalyzer(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, @PathParam("analyzer_name") String analyzer_name,
-					AnalyzerDefinition analyzer);
+			@PathParam("index_name") String index_name, @PathParam("analyzer_name") String analyzer_name,
+			AnalyzerDefinition analyzer);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/analyzers")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	LinkedHashMap<String, AnalyzerDefinition> setAnalyzers(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, LinkedHashMap<String, AnalyzerDefinition> analyzers);
+			@PathParam("index_name") String index_name, LinkedHashMap<String, AnalyzerDefinition> analyzers);
 
 	@DELETE
 	@Path("/{schema_name}/{index_name}/analyzers/{analyzer_name}")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response deleteAnalyzer(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					@PathParam("analyzer_name") String analyzer_name);
+			@PathParam("analyzer_name") String analyzer_name);
 
 	@GET
 	@Path("/{schema_name}/{index_name}")
@@ -173,60 +172,60 @@ public interface IndexServiceInterface extends ServiceInterface {
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response postDocument(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					Map<String, Object> document);
+			Map<String, Object> document);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/docs")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response postDocuments(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					List<Map<String, Object>> documents);
+			List<Map<String, Object>> documents);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/doc/values")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response updateDocumentValues(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, Map<String, Object> document);
+			@PathParam("index_name") String index_name, Map<String, Object> document);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/docs/values")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Response updateDocumentsValues(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, List<Map<String, Object>> documents);
+			@PathParam("index_name") String index_name, List<Map<String, Object>> documents);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/backup")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	BackupStatus doBackup(@PathParam("schema_name") String schema_name, @PathParam("index_name") String index_name,
-					@QueryParam("keep_last") Integer keep_last_count);
+			@QueryParam("keep_last") Integer keep_last_count);
 
 	@GET
 	@Path("/{schema_name}/{index_name}/backup")
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	List<BackupStatus> getBackups(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name);
+			@PathParam("index_name") String index_name);
 
 	@POST
 	@Path("/{schema_name}/{index_name}/search")
 	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
 	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	ResultDefinition searchQuery(@PathParam("schema_name") String schema_name,
-					@PathParam("index_name") String index_name, QueryDefinition query,
-					@QueryParam("delete") Boolean delete);
+			@PathParam("index_name") String index_name, QueryDefinition query, @QueryParam("delete") Boolean delete);
 
 	public static IndexServiceInterface getClient(Boolean local, String group, Integer msTimeout)
-					throws URISyntaxException {
+			throws URISyntaxException {
 		if (local != null && local)
 			return new IndexServiceImpl();
+		if (!ClusterManager.INSTANCE.isCluster())
+			return new IndexServiceImpl();
 		String[] nodes = ClusterManager.INSTANCE.getClusterClient()
-						.getActiveNodesByService(IndexManager.SERVICE_NAME_SEARCH, group);
+				.getActiveNodesByService(IndexManager.SERVICE_NAME_SEARCH, group);
 		if (nodes == null)
 			throw new RuntimeException("Index service not available");
 		if (nodes.length == 1)
 			return new IndexSingleClient(nodes[0], msTimeout);
-		return new IndexMultiClient(ClusterManager.INSTANCE.executor, ClusterManager.INSTANCE.getClusterClient()
-						.getActiveNodesByService(IndexManager.SERVICE_NAME_SEARCH, group), msTimeout);
+		return new IndexMultiClient(ClusterManager.INSTANCE.executor, nodes, msTimeout);
 	}
 }
