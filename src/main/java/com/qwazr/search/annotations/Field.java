27571102e7271c9e5daf1201d01d5113fc6a753f
Emmanuel Keller <ekeller@open-search-server.com>
2016-03-14 17:03:47 +0100

    Add Field annotations for annotated documents

---
+++ src/main/java/com/qwazr/search/annotations/Field.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2016 Emmanuel Keller / QWAZR
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qwazr.search.annotations;
+
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.index.DocValuesType;
+import org.apache.lucene.index.IndexOptions;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+@interface Field {
+
+	String analyzer();
+
+	String queryAnalyzer();
+
+	boolean tokenized() default false;
+
+	boolean stored() default false;
+
+	boolean storeTermVectors() default false;
+
+	boolean storeTermVectorOffsets() default false;
+
+	boolean storeTermVectorPositions() default false;
+
+	boolean storeTermVectorPayloads() default false;
+
+	boolean omitNorms() default false;
+
+	FieldType.NumericType numericType();
+
+	IndexOptions indexOptions();
+
+	DocValuesType docValuesType();
+
+}
