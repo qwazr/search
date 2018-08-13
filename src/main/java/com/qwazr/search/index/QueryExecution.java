4cdf54eb1843ec7e6a16001da78470fe73c7eee1
Emmanuel Keller <ekeller@open-search-server.com>
2018-08-13 11:53:42 +0200

    Fix OOM on large paging values #15

--- src/main/java/com/qwazr/search/index/QueryExecution.java
+++ src/main/java/com/qwazr/search/index/QueryExecution.java
@@ -42,124 +42,124 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 final class QueryExecution<T extends ResultDocumentAbstract> {
 
-	final QueryContextImpl queryContext;
-	final QueryDefinition queryDef;
-	final TimeTracker timeTracker;
-	final Map<String, String> dimensions;
-	final FacetsConfig facetsConfig;
-	final int start;
-	final int rows;
-	final int end;
-	final Sort sort;
-	final boolean bNeedScore;
-	final boolean useDrillSideways;
-	final Query query;
-	final List<Pair<Constructor, Object[]>> collectorConstructors;
+    final QueryContextImpl queryContext;
+    final QueryDefinition queryDef;
+    final TimeTracker timeTracker;
+    final Map<String, String> dimensions;
+    final FacetsConfig facetsConfig;
+    final int start;
+    final int rows;
+    final int end;
+    final Sort sort;
+    final boolean bNeedScore;
+    final boolean useDrillSideways;
+    final Query query;
+    final List<Pair<Constructor, Object[]>> collectorConstructors;
 
-	private final boolean isConcurrent;
+    private final boolean isConcurrent;
 
-	QueryExecution(final QueryContextImpl queryContext, final QueryDefinition queryDefinition)
-			throws QueryNodeException, ReflectiveOperationException, ParseException, IOException {
+    QueryExecution(final QueryContextImpl queryContext, final QueryDefinition queryDefinition)
+            throws QueryNodeException, ReflectiveOperationException, ParseException, IOException {
 
-		this.timeTracker = new TimeTracker();
+        this.timeTracker = new TimeTracker();
 
-		this.queryContext = queryContext;
-		this.queryDef = queryDefinition;
+        this.queryContext = queryContext;
+        this.queryDef = queryDefinition;
 
-		this.query = queryDef.luceneQuery != null ?
-				queryDef.luceneQuery :
-				queryDef.query == null ? new MatchAllDocsQuery() : queryDef.query.getQuery(queryContext);
+        this.query = queryDef.luceneQuery != null ?
+                queryDef.luceneQuery :
+                queryDef.query == null ? new MatchAllDocsQuery() : queryDef.query.getQuery(queryContext);
 
-		this.sort = queryDef.sorts == null ? null : SortUtils.buildSort(queryContext.fieldMap, queryDef.sorts);
+        this.sort = queryDef.sorts == null ? null : SortUtils.buildSort(queryContext.fieldMap, queryDef.sorts);
 
-		this.dimensions = queryDef.facets == null ? null : FacetsBuilder.getFields(queryDef.facets);
-		this.facetsConfig = dimensions == null ? null : queryContext.fieldMap.getFacetsConfig(dimensions);
+        this.dimensions = queryDef.facets == null ? null : FacetsBuilder.getFields(queryDef.facets);
+        this.facetsConfig = dimensions == null ? null : queryContext.fieldMap.getFacetsConfig(dimensions);
 
-		this.start = queryDef.getStartValue();
-		this.rows = queryDef.getRowsValue();
-		this.end = start + rows;
+        this.start = queryDef.getStartValue();
+        this.rows = queryDef.getRowsValue();
+        this.end = Math.min(start + rows, queryContext.indexReader.numDocs());
 
-		this.bNeedScore = sort == null || sort.needsScores();
-		this.useDrillSideways =
-				queryDef.query instanceof DrillDownQuery && ((DrillDownQuery) queryDef.query).useDrillSideways &&
-						queryDef.facets != null;
-		if (queryDef.collectors != null && !queryDef.collectors.isEmpty()) {
-			collectorConstructors = new ArrayList<>();
-			isConcurrent = buildExternalCollectors(queryDef.collectors, collectorConstructors);
-		} else {
-			collectorConstructors = null;
-			isConcurrent = true;
-		}
-	}
+        this.bNeedScore = sort == null || sort.needsScores();
+        this.useDrillSideways =
+                queryDef.query instanceof DrillDownQuery && ((DrillDownQuery) queryDef.query).useDrillSideways &&
+                        queryDef.facets != null;
+        if (queryDef.collectors != null && !queryDef.collectors.isEmpty()) {
+            collectorConstructors = new ArrayList<>();
+            isConcurrent = buildExternalCollectors(queryDef.collectors, collectorConstructors);
+        } else {
+            collectorConstructors = null;
+            isConcurrent = true;
+        }
+    }
 
-	private static boolean buildExternalCollectors(final Map<String, QueryDefinition.CollectorDefinition> collectors,
-			final List<Pair<Constructor, Object[]>> collectorConstructors) throws ReflectiveOperationException {
-		if (collectors == null || collectors.isEmpty())
-			return true; // By default we use concurrent
-		final AtomicInteger concurrentCollectors = new AtomicInteger(0);
-		final AtomicInteger classicCollectors = new AtomicInteger(0);
-		ConcurrentUtils.forEachEx(collectors, (name, collector) -> {
-			final Class<? extends Collector> collectorClass = ClassLoaderUtils.findClass(collector.classname);
-			Constructor<?>[] constructors = collectorClass.getConstructors();
-			if (constructors.length == 0)
-				throw new ReflectiveOperationException("No constructor for class: " + collectorClass);
-			final Constructor<?> constructor;
-			final Object[] arguments;
-			if (collector.arguments == null || collector.arguments.length == 0) {
-				constructor = collectorClass.getConstructor(String.class);
-				arguments = new Object[] { name };
-			} else {
-				arguments = new Object[collector.arguments.length + 1];
-				arguments[0] = name;
-				System.arraycopy(collector.arguments, 0, arguments, 1, collector.arguments.length);
-				Class[] classes = new Class[arguments.length];
-				int i = 0;
-				for (Object arg : arguments)
-					classes[i++] = arg.getClass();
-				constructor = collectorClass.getConstructor(classes);
-			}
-			collectorConstructors.add(Pair.of(constructor, arguments));
-			if (ConcurrentCollector.class.isAssignableFrom(collectorClass))
-				concurrentCollectors.incrementAndGet();
-			else
-				classicCollectors.incrementAndGet();
-		});
-		if (concurrentCollectors.get() > 0 && classicCollectors.get() > 0)
-			throw new IllegalArgumentException("Cannot mix concurrent collectors and classic collectors");
-		return concurrentCollectors.get() > 0 || classicCollectors.get() == 0;
-	}
+    private static boolean buildExternalCollectors(final Map<String, QueryDefinition.CollectorDefinition> collectors,
+                                                   final List<Pair<Constructor, Object[]>> collectorConstructors) throws ReflectiveOperationException {
+        if (collectors == null || collectors.isEmpty())
+            return true; // By default we use concurrent
+        final AtomicInteger concurrentCollectors = new AtomicInteger(0);
+        final AtomicInteger classicCollectors = new AtomicInteger(0);
+        ConcurrentUtils.forEachEx(collectors, (name, collector) -> {
+            final Class<? extends Collector> collectorClass = ClassLoaderUtils.findClass(collector.classname);
+            Constructor<?>[] constructors = collectorClass.getConstructors();
+            if (constructors.length == 0)
+                throw new ReflectiveOperationException("No constructor for class: " + collectorClass);
+            final Constructor<?> constructor;
+            final Object[] arguments;
+            if (collector.arguments == null || collector.arguments.length == 0) {
+                constructor = collectorClass.getConstructor(String.class);
+                arguments = new Object[]{name};
+            } else {
+                arguments = new Object[collector.arguments.length + 1];
+                arguments[0] = name;
+                System.arraycopy(collector.arguments, 0, arguments, 1, collector.arguments.length);
+                Class[] classes = new Class[arguments.length];
+                int i = 0;
+                for (Object arg : arguments)
+                    classes[i++] = arg.getClass();
+                constructor = collectorClass.getConstructor(classes);
+            }
+            collectorConstructors.add(Pair.of(constructor, arguments));
+            if (ConcurrentCollector.class.isAssignableFrom(collectorClass))
+                concurrentCollectors.incrementAndGet();
+            else
+                classicCollectors.incrementAndGet();
+        });
+        if (concurrentCollectors.get() > 0 && classicCollectors.get() > 0)
+            throw new IllegalArgumentException("Cannot mix concurrent collectors and classic collectors");
+        return concurrentCollectors.get() > 0 || classicCollectors.get() == 0;
+    }
 
-	final ResultDefinition<T> execute(final ResultDocuments<T> resultDocuments) throws Exception {
+    final ResultDefinition<T> execute(final ResultDocuments<T> resultDocuments) throws Exception {
 
-		final ResultDocumentsInterface resultDocumentsInterface = resultDocuments.getResultDocuments();
+        final ResultDocumentsInterface resultDocumentsInterface = resultDocuments.getResultDocuments();
 
-		final QueryCollectors queryCollectors =
-				isConcurrent ? new QueryCollectorManager(this) : new QueryCollectorsClassic(this);
+        final QueryCollectors queryCollectors =
+                isConcurrent ? new QueryCollectorManager(this) : new QueryCollectorsClassic(this);
 
-		final FacetsBuilder facetsBuilder = queryCollectors.execute();
+        final FacetsBuilder facetsBuilder = queryCollectors.execute();
 
-		final TopDocs topDocs = queryCollectors.getTopDocs();
-		final Integer totalHits = queryCollectors.getTotalHits();
+        final TopDocs topDocs = queryCollectors.getTopDocs();
+        final Integer totalHits = queryCollectors.getTotalHits();
 
-		final Map<String, HighlighterImpl> highlighters;
-		if (queryDef.highlighters != null && topDocs != null) {
-			highlighters = new LinkedHashMap<>();
-			queryDef.highlighters.forEach((name, highlighterDefinition) -> highlighters.put(name,
-					new HighlighterImpl(name, highlighterDefinition, queryContext)));
-		} else
-			highlighters = null;
+        final Map<String, HighlighterImpl> highlighters;
+        if (queryDef.highlighters != null && topDocs != null) {
+            highlighters = new LinkedHashMap<>();
+            queryDef.highlighters.forEach((name, highlighterDefinition) -> highlighters.put(name,
+                    new HighlighterImpl(name, highlighterDefinition, queryContext)));
+        } else
+            highlighters = null;
 
-		timeTracker.next("search_query");
+        timeTracker.next("search_query");
 
-		final ResultDocumentsBuilder resultBuilder =
-				new ResultDocumentsBuilder(queryDef, topDocs, queryContext.indexSearcher, query, highlighters,
-						queryCollectors.getExternalResults(), timeTracker, facetsBuilder,
-						totalHits == null ? 0 : totalHits, resultDocumentsInterface);
+        final ResultDocumentsBuilder resultBuilder =
+                new ResultDocumentsBuilder(queryDef, topDocs, queryContext.indexSearcher, query, highlighters,
+                        queryCollectors.getExternalResults(), timeTracker, facetsBuilder,
+                        totalHits == null ? 0 : totalHits, resultDocumentsInterface);
 
-		return resultDocuments.apply(resultBuilder);
-	}
+        return resultDocuments.apply(resultBuilder);
+    }
 
-	final Explanation explain(final int docId) throws IOException {
-		return queryContext.indexSearcher.explain(query, docId);
-	}
+    final Explanation explain(final int docId) throws IOException {
+        return queryContext.indexSearcher.explain(query, docId);
+    }
 }
