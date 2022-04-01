fd0055fdcba38baeabee6ca7d8b11bc0bd017f32
Emmanuel Keller <ekeller@open-search-server.com>
2022-04-01 12:12:01 +0100

    remove old ui interface Restore docs location

--- qwazr-search/src/main/ui/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './qwazr.css'
-
-import App from './App';
-
-ReactDOM.render(<React.StrictMode><App/></React.StrictMode>, document.getElementById("root"));
